201da16cc466bb9c7e6372166241fbb613f33a63
Genna Wingert <wingertbe@gmail.com>
2020-03-21 22:43:50 +0100

    Update wasm.rs

--- src/wasm.rs
+++ src/wasm.rs
@@ -74,7 +74,7 @@ impl Future for InnerFuture {
 }
 
 mod fetch {
-    use js_sys::{Array, ArrayBuffer, Reflect, Uint8Array, JsCast};
+    use js_sys::{Array, ArrayBuffer, Reflect, Uint8Array};
     use wasm_bindgen::JsCast;
     use wasm_bindgen_futures::JsFuture;
     use web_sys::{RequestInit, Window};
